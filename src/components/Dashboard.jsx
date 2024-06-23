e573396410ef41279950293b9aa46d88f082b983
Naga Nithin <yuvanaganithin@gmail.com>
2024-06-24 02:25:36 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -147,7 +147,7 @@ const Dashboard = () => {
   // Set up socket.io connection
   useEffect(() => {
     console.log("Initializing WebSocket connection...");
-    socket.current = io("https://api.chatwave.one", {
+    socket.current = io("https://api.chatwave.one:9092", {
       path: "/socket.io",
       transports: ["websocket"],
       query: {
